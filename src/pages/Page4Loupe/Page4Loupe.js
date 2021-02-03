890005604c2523296ee4806b77886ad59392ed45
valentingnt <valentin64.genest@gmail.com>
2021-02-03 17:12:19 +0100

    -f

--- src/pages/Page4Loupe/Page4Loupe.js
+++ src/pages/Page4Loupe/Page4Loupe.js
@@ -1,10 +1,20 @@
-import styles from "./Page4Loupe.module.css";
-import React from 'react'
+import styles from "./Page4Loupe.module.scss";
+import React from 'react';
+import BoxText from '../../components/BoxText/BoxText';
+import tableau4 from '../../img/tableau4.jpg';
+
 
 const Page4Loupe = () => {
   return (
     <div className={styles.containerPage4Loupe}>
-      Page4Loupe
+
+      <img src={tableau4} alt="Tableau 4" />
+
+        <BoxText
+          namePerso="Vous"
+          textBoxContenu="Une page secrète ! Jetons un œil au carnet pour la trouver et découvrir ce qu'elle contient."
+        />
+        
     </div>
   );
 };
