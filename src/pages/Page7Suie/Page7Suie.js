d9e1be10b9e9eca17e0c1f7477e9d4c66b122bd3
Bright2196 <claireriussetcr@gmail.com>
2021-02-03 14:52:37 +0100

    page7

--- src/pages/Page7Suie/Page7Suie.js
+++ src/pages/Page7Suie/Page7Suie.js
@@ -1,7 +1,7 @@
 import styles from "./Page7Suie.module.css";
 import React from 'react'
 import InputText from "../../components/InputCode/InputCode";
-
+import imagePage7 from "./img/tableauPage7.jpg"
 
 const Page7Suie = () => {
   return (
@@ -10,6 +10,9 @@ const Page7Suie = () => {
               <InputText
                   placeholderInput="Saisir le code ici"
               />
+        </div >
+        <div className={styles.ContainerImage}>
+            <img src={imagePage7} alt=""/>
         </div>
     </div>
   );
