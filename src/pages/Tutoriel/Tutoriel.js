75a539625debae7338fe4b6decd996bf4f015c2e
valentingnt <valentin64.genest@gmail.com>
2021-02-03 14:50:59 +0100

    fixed header

--- src/pages/Tutoriel/Tutoriel.js
+++ src/pages/Tutoriel/Tutoriel.js
@@ -8,12 +8,12 @@ const Tutoriel = () => {
       <p className={styles.tutorielText}>Déplacez votre curseur sur les pages du carnet et sur les tableaux. 
           Soyez attentifs aux détails et n'hésitez pas à fouiller! 
           Résolvez le mystère de ce carnet…</p>
-    <div className={styles.buttonContainer}>
+      <div className={styles.buttonContainer}>
         <NormalButton
             buttonText="J'ai compris !"
             link=""
         />
-    </div>
+      </div>
     </div>
   );
 };
