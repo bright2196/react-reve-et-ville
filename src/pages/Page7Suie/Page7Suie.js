4620b64295693acfd0c25bc5074983204d827361
Bright2196 <claireriussetcr@gmail.com>
2021-02-03 14:51:37 +0100

    page7Suie

--- src/pages/Page7Suie/Page7Suie.js
+++ src/pages/Page7Suie/Page7Suie.js
@@ -1,10 +1,16 @@
 import styles from "./Page7Suie.module.css";
 import React from 'react'
+import InputText from "../../components/InputCode/InputCode";
+
 
 const Page7Suie = () => {
   return (
     <div className={styles.containerPage7Suie}>
-      Page7Suie
+        <div className={styles.ContainerInput}>
+              <InputText
+                  placeholderInput="Saisir le code ici"
+              />
+        </div>
     </div>
   );
 };
