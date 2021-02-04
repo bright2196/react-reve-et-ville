2a3809dc4a4da9dfa7ba6d05b583498d3fa2470f
Bright2196 <claireriussetcr@gmail.com>
2021-02-04 13:36:26 +0100

    fix bug header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -55,7 +55,7 @@ const Header = () => {
         
                 <button>
                     <div className={styles.stack}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#4C2209" strokeWidth="2" strokeLinecap="2" strokeLinecap="round" strokeLinejoin="round"><path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#4C2209" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path></svg>
                     <p>Galerie</p>
                     </div>
                 </button>
