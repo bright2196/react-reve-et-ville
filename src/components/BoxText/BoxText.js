7752a776db0083f5a5ef061fdc520bb8500aa5b6
antoinetardivel <tardivelantoine@gmail.com>
2021-02-04 21:39:49 +0100

    affichage machine à écrire

--- src/components/BoxText/BoxText.js
+++ src/components/BoxText/BoxText.js
@@ -1,20 +1,62 @@
 import React from "react";
 import styles from './BoxText.module.scss';
 import PropTypes from 'prop-types';
+import Typewriter from 'typewriter-effect';
+
+// const [written, setwritten] = useState(false);
+
 class BoxText extends React.Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      written: false
+    };
+  }
   render() {
     return (
-    <div className= {styles.ContainerBox}>
+    <div id="app" className= {styles.ContainerBox}>
             <div className= {styles.Containertext}>
-                <label className={styles.namePerso}>{this.props.namePerso}</label>
-                <p className={styles.corpsBoxText}>{this.props.textBoxContenu}</p>
+                <label className={styles.namePerso}>
+                  <Typewriter
+                    options={{
+                      delay: 20,
+                      cursorClassName: 'Cursor-BoxTex',
+                    }}
+                    onInit={(typewriter => {
+                      typewriter
+                        .typeString(this.props.namePerso)
+                        .callFunction(() => {
+                          document.getElementsByClassName('Cursor-BoxTex')[0].style.display = "none";
+                          // console.log(cursorlist)
+                          this.setState({ written: true })
+                        })
+                        .start();
+                    })}
+                  />
+                </label>
+                <p className={styles.corpsBoxText}>
+                  {this.state.written &&
+                    <Typewriter
+                      options={{
+                        delay: 20,
+                        cursorClassName: 'Cursor-BoxTex2',
+                      }}
+                      onInit={(typewriter) => {
+                        typewriter
+                          .typeString(this.props.textBoxContenu)
+                          .callFunction(() => {
+                            document.getElementsByClassName('Cursor-BoxTex2')[0].style.display = "none";
+                            // console.log(cursorlist)
+                          })
+                          .start();
+                      }}
+                    />
+                  }
+                </p>
+                
             </div>
     
       </div>
-   
-     
-      
     );
   }
 }
@@ -26,3 +68,20 @@ BoxText.propTypes = {
   }
 
  export default BoxText;
+
+
+
+// <Typewriter
+//   onInit={(typewriter) => {
+//     typewriter.typeString('Hello World!')
+//       .callFunction(() => {
+//         console.log('String typed out!');
+//       })
+//       .pauseFor(2500)
+//       .deleteAll()
+//       .callFunction(() => {
+//         console.log('All strings were deleted');
+//       })
+//       .start();
+//   }}
+// />
