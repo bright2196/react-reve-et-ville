c25c5e81b14d568573c684bba9bdcc73d3c1e693
Bright2196 <claireriussetcr@gmail.com>
2021-02-04 13:46:22 +0100

    fix flashlight

--- src/pages/Page7Suie/Page7Suie.js
+++ src/pages/Page7Suie/Page7Suie.js
@@ -33,9 +33,7 @@ const Page7Suie = () => {
               />
         </div >
     <div className={styles.imagePage7}>
-        <ReactFlashlight
-        
-        showCursor={false}>
+        <ReactFlashlight>
           <div className={styles.imagePage7} style={style}>
            
           </div>
