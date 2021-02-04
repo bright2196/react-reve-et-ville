c91dce44d829d1b22741f887180c27c123b1d9e4
Maelle05 <maelle.rabouan@gmail.com>
2021-02-04 10:29:55 +0100

    rectif des erreurs pour netlify

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
               <p className={styles.homedesc}>Lancez vous dans une course aux indices et plongez vous dans les toiles du Musée des Beaux Arts de Bordeaux pour mettre au jour un secret bien gardé.</p>
             </div>
             <div className={styles.beginbutton}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="100" height="1" viewBox="0 0 100 1"><defs><style></style></defs><line class="a" x1="100" transform="translate(0 0.5)"/></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="100" height="1" viewBox="0 0 100 1"><defs><style></style></defs><line className="a" x1="100" transform="translate(0 0.5)"/></svg>
               <Link
                 href="tutoriel"
               >
@@ -21,7 +21,7 @@ const Home = () => {
                     link=""
                 />
               </Link>        
-              <svg xmlns="http://www.w3.org/2000/svg" width="100" height="1" viewBox="0 0 100 1"><defs><style></style></defs><line class="a" x1="100" transform="translate(0 0.5)"/></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="100" height="1" viewBox="0 0 100 1"><defs><style></style></defs><line className="a" x1="100" transform="translate(0 0.5)"/></svg>
             </div>
         </div>
   );
