46777253ab942070d1cdc2bb4e1314a27389d2d6
Bright2196 <claireriussetcr@gmail.com>
2021-02-05 12:29:50 +0100

    page musee finish

--- src/pages/Page2Musee/Page2Musee.js
+++ src/pages/Page2Musee/Page2Musee.js
@@ -1,7 +1,6 @@
 import styles from "./Page2Musee.module.scss";
 import React from "react";
 import BoxText from "../../components/BoxText/BoxText";
-import NormalButton from "../../components/NormalButton/NormalButton";
 import Link from "../../components/Link/Link";
 import BackgroundImg from "../../components/BackgroundImage/BackgroundImage";
 import musee from "../../img/tableaux/MuseeEdit.jpg";
@@ -13,18 +12,19 @@ const Page2Musee = () => {
       <Header colorIcones={true}/>
       <div className={styles.containerPage2Musee}>
         <BackgroundImg ImageNum={2} Blur={true} BrownColor={true}/>
-        <img className={styles.tableau} src={musee} alt="Musée Enigme" />
+        
+        <div className={styles.containerImage}>
+               <img className={styles.tableau} src={musee} alt="Musée Enigme" />
+              <Link href="pageNatureMorte" >
+              <div className={styles.clickableArea} ></div>
+              </Link>
+            </div>
+       
         <BoxText
-          textBoxContenu="Le regard du cavalier te guidera... Qu’est ce que cela veut bien pouvoir dire ?"
+          textBoxContenu='Maintenant que je suis au bon endroit, voyons si la note laissée par mon grand père peut être utile. "Le regard du cavalier te guidera..." Qu’est ce que cela veut bien pouvoir dire ?'
           namePerso="Vous"
         />
-        <div className={styles.divProvisoire}>
-          <Link href="pageNatureMorte">
-            <NormalButton
-              buttonText="J'ai cliqué sur le tableaux de la nature morte, je passe à la suite ->"
-            />
-          </Link>
-        </div>
+        
       </div>
     </div>
   );
