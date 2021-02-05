62ed9c4408b27a0ca7942cede99f30876d011ebe
valentingnt <valentin64.genest@gmail.com>
2021-02-05 12:54:14 +0100

    haha

--- src/pages/Page3NatureMorte/Page3NatureMorte.js
+++ src/pages/Page3NatureMorte/Page3NatureMorte.js
@@ -11,6 +11,23 @@ const Page3NatureMorte = () => {
   return (
     <div>
       <Header colorIcones={true}/>
+      <div className={styles.boutonSuivantContainer}>
+            <Link href="pageLoupe" >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="68" height="68" viewBox="0 0 68 68">
+                        <g id="Groupe_31" data-name="Groupe 31" transform="translate(-1233 -337)">
+                        <g id="Ellipse_9" data-name="Ellipse 9" transform="translate(1233 337)" fill="none" stroke="#FFFDF1" stroke-width="1">
+                        <circle cx="34" cy="34" r="34" stroke="none"/>
+                        <circle cx="34" cy="34" r="33.5" fill="none"/>
+                        </g>
+                        <g id="Groupe_16" data-name="Groupe 16" transform="translate(1252 377) rotate(-90)">
+                        <path id="Tracé_57" data-name="Tracé 57" d="M0,31V0" transform="translate(6)" fill="none" stroke="#FFFDF1" stroke-width="1"/>
+                        <path id="Tracé_50" data-name="Tracé 50" d="M6,10A19.413,19.413,0,0,0,3.975,3.9,12.233,12.233,0,0,0,0,0" transform="translate(0 21)" fill="none" stroke="#FFFDF1" stroke-width="1"/>
+                        <path id="Tracé_51" data-name="Tracé 51" d="M0,10A19.413,19.413,0,0,1,2.025,3.9,12.233,12.233,0,0,1,6,0" transform="translate(6 21)" fill="none" stroke="#FFFDF1" stroke-width="1"/>
+                        </g>
+                    </g>
+                    </svg>
+            </Link>
+      </div>
       <div className={styles.containerPage3NatureMorte}>
         <BackgroundImg ImageNum={2} Blur={true}/>
         <img src={naturemorte} className={styles.tableau} alt="Nature Morte Enigme"/>
@@ -19,9 +36,6 @@ const Page3NatureMorte = () => {
           namePerso="Vous"
         />
         <div className={styles.divProvisoire}>
-        <Link href="pageLoupe" >
-          <NormalButton buttonText="Je veux aller à la loupe, voir la suite ->" />
-        </Link >
         </div>
       </div>
     </div>
