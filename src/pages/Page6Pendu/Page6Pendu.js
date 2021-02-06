ec6d56fb64a20582c66472850d604ddfcc245eb1
Maelle05 <maelle.rabouan@gmail.com>
2021-02-06 21:38:33 +0100

    bouton pendu sur img fleche

--- src/pages/Page6Pendu/Page6Pendu.js
+++ src/pages/Page6Pendu/Page6Pendu.js
@@ -1,7 +1,7 @@
 import styles from "./Page6Pendu.module.scss";
 import React from 'react'
 import Link from '../../components/Link/Link'
-import NormalButton from "../../components/NormalButton/NormalButton";
+// import NormalButton from "../../components/NormalButton/NormalButton";
 import BoxText from "../../components/BoxText/BoxText";
 import BackgroundImg from "../../components/BackgroundImage/BackgroundImage"
 import Header from '../../components/Header/Header'
@@ -29,9 +29,9 @@ class Page6Pendu extends React.Component {
         <div className={styles.hintTuto}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="12.492" height="31.072" viewBox="0 0 12.492 31.072">
             <g id="Groupe_16" data-name="Groupe 16" transform="translate(0.246)">
-                <path id="Tracé_57" data-name="Tracé 57" d="M0,31V0" transform="translate(6)" fill="none" stroke="#4c2209" strokeWidth="1"/>
-                <path id="Tracé_50" data-name="Tracé 50" d="M6,10A19.413,19.413,0,0,0,3.975,3.9,12.233,12.233,0,0,0,0,0" transform="translate(0 21)" fill="none" stroke="#4c2209" strokeWidth="1"/>
-                <path id="Tracé_51" data-name="Tracé 51" d="M0,10A19.413,19.413,0,0,1,2.025,3.9,12.233,12.233,0,0,1,6,0" transform="translate(6 21)" fill="none" stroke="#4c2209" strokeWidth="1"/>
+                <path id="Tracé_57" data-name="Tracé 57" d="M0,31V0" transform="translate(6)" fill="none" stroke="#4C2209" strokeWidth="1"/>
+                <path id="Tracé_50" data-name="Tracé 50" d="M6,10A19.413,19.413,0,0,0,3.975,3.9,12.233,12.233,0,0,0,0,0" transform="translate(0 21)" fill="none" stroke="#4C2209" strokeWidth="1"/>
+                <path id="Tracé_51" data-name="Tracé 51" d="M0,10A19.413,19.413,0,0,1,2.025,3.9,12.233,12.233,0,0,1,6,0" transform="translate(6 21)" fill="none" stroke="#4C2209" strokeWidth="1"/>
             </g>
             </svg>
               <p className={styles.hintTextTuto}>Peut-être que la page secrète que vous avez découverte peut vous aider sur cette énigme…</p>
@@ -46,25 +46,26 @@ class Page6Pendu extends React.Component {
                     <img src={noteWin} className={styles.img} alt=""/>
                     <div className={styles.btnContainer}>
                     <BoxText textBoxContenu='Super ! Le mot secret était "Tableaux" ! Et il doit donc y en avoir treize sur la toile suivante... Mais je me demande bien qui peut-être ce fidèle compagnon dont la musique parle...'></BoxText>
-                      <div className={styles.btn}><Link href="pageSalon" >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="68" height="68" viewBox="0 0 68 68">
-                        <g id="Groupe_31" data-name="Groupe 31" transform="translate(-1233 -337)">
-                        <g id="Ellipse_9" data-name="Ellipse 9" transform="translate(1233 337)" fill="none" stroke="#FFFDF1" strokeWidth="1">
-                        <circle cx="34" cy="34" r="34" stroke="none"/>
-                        <circle cx="34" cy="34" r="33.5" fill="none"/>
+                    <div className={styles.boutonSuivantContainer}>
+                    <Link href="pageSalon" >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="68" height="68" viewBox="0 0 68 68">
+                            <g id="Groupe_31" data-name="Groupe 31" transform="translate(-1233 -337)">
+                            <g id="Ellipse_9" data-name="Ellipse 9" transform="translate(1233 337)" fill="none" stroke="#4C2209" strokeWidth="1">
+                            <circle cx="34" cy="34" r="34" stroke="none"/>
+                            <circle cx="34" cy="34" r="33.5" fill="none"/>
+                            </g>
+                            <g id="Groupe_16" data-name="Groupe 16" transform="translate(1252 377) rotate(-90)">
+                            <path id="Tracé_57" data-name="Tracé 57" d="M0,31V0" transform="translate(6)" fill="none" stroke="#4C2209" strokeWidth="1"/>
+                            <path id="Tracé_50" data-name="Tracé 50" d="M6,10A19.413,19.413,0,0,0,3.975,3.9,12.233,12.233,0,0,0,0,0" transform="translate(0 21)" fill="none" stroke="#4C2209" strokeWidth="1"/>
+                            <path id="Tracé_51" data-name="Tracé 51" d="M0,10A19.413,19.413,0,0,1,2.025,3.9,12.233,12.233,0,0,1,6,0" transform="translate(6 21)" fill="none" stroke="#4C2209" strokeWidth="1"/>
+                            </g>
                         </g>
-                        <g id="Groupe_16" data-name="Groupe 16" transform="translate(1252 377) rotate(-90)">
-                        <path id="Tracé_57" data-name="Tracé 57" d="M0,31V0" transform="translate(6)" fill="none" stroke="#FFFDF1" strokeWidth="1"/>
-                        <path id="Tracé_50" data-name="Tracé 50" d="M6,10A19.413,19.413,0,0,0,3.975,3.9,12.233,12.233,0,0,0,0,0" transform="translate(0 21)" fill="none" stroke="#FFFDF1" strokeWidth="1"/>
-                        <path id="Tracé_51" data-name="Tracé 51" d="M0,10A19.413,19.413,0,0,1,2.025,3.9,12.233,12.233,0,0,1,6,0" transform="translate(6 21)" fill="none" stroke="#FFFDF1" strokeWidth="1"/>
-                        </g>
-                    </g>
-                    </svg>
-            </Link >
+                        </svg>
+                      </Link >
                       </div>
                     </div>
                   </div>
-                </div> 
+                </div>
             )
             :
             (
