26ffd44b2c734f4b2e375fe0d3ce04696a3dbfb3
Maelle05 <maelle.rabouan@gmail.com>
2021-03-07 21:38:45 +0100

    changement du btn song

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,6 @@ import React from 'react'
 import Carnet from "../Carnet/Carnet";
 import PropTypes from 'prop-types';
 import Indices from '../Indices/Indices'
-import Song from '../../audio/Musique.mp3'
 import SongB from '../../audio/Bouton.mp3'
 
 class Header extends React.Component {
@@ -37,40 +36,6 @@ class Header extends React.Component {
     return(
         <div>
             <div className={styles.header}>
-                <audio loop src={Song} id='musiqueSite' onLoadStart={()=>{
-                const audioSite = document.getElementById('musiqueSite')
-                audioSite.play()
-                }} />
-                {
-                   this.props.allowCarnet ? (<audio src={SongB} id='audioBouton' onLoadStart={()=>{
-                    const boutonAudio = document.getElementById('audioBouton')
-                    boutonAudio.play()
-                }} />) : (<div></div>)
-                }
-                
-                <div className={styles.svgson}>
-                
-                {
-                    !this.state.setSong?(
-                        <button onClick={()=> {
-                            const audioSite = document.getElementById('musiqueSite')
-                            audioSite.play()
-                            this.setState({ setSong: true })
-                            }}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" ><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon><line x1="23" y1="9" x2="17" y2="15"></line><line x1="17" y1="9" x2="23" y2="15"></line></svg>
-                        </button>
-                    ):(
-                       <button onClick={()=> {
-                        const audioSite = document.getElementById('musiqueSite')
-                        audioSite.pause()
-                        this.setState({ setSong: false })
-                        }}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" ><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon><path d="M19.07 4.93a10 10 0 0 1 0 14.14M15.54 8.46a5 5 0 0 1 0 7.07"></path></svg>
-                        </button> 
-                    )
-                }      
-            </div>
-    
                 <div className={styles.pictosdroite}>
                     <button id='btn-carnet' onClick={() => {
                         if(this.props.allowCarnet){
@@ -107,18 +72,7 @@ class Header extends React.Component {
                             this.props.colorIcones === true ? (<p className={styles.pictoB}>Indices</p>) : (<p>Indices</p>)
                         }
                     </div>
-                </button>
-        
-        
-                <button>
-                    <div className={styles.stack} onClick={()=> {alert('La galerie n\'est pas encore disponible')}}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path></svg>
-                    {
-                        this.props.colorIcones === true ? (<p className={styles.pictoB}>Galerie</p>) : (<p>Galerie</p>)
-                    }
-                    </div>
-                </button>
-        
+                </button>      
         
                 </div>
         
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App/App.js'
+import SongComp from './components/HeaderSondBotton/HeaderSondBotton'
 
 ReactDOM.render(
   <React.StrictMode>
+      <SongComp />
       <App />
   </React.StrictMode>,
   document.getElementById('root')
