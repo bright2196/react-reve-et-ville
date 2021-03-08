2c7f9632e6d7544e94c451850f3130d4a01f771e
Maelle05 <maelle.rabouan@gmail.com>
2021-03-08 13:36:13 +0100

    liens vers portfolio

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -34,12 +34,12 @@ const Home = () => {
                 )
               }
                       
-              <p className={styles.homeprenom}>Expérience réalisée par :  <a href='https://www.justinemagry.fr/' target="_blank" className={styles.link}> Justine Magry</a> , 
-                                                                          <a href='https://hugoblanchard.com/fr/index-fr.html' target="_blank" className={styles.link}> Hugo Blanchard</a> , 
-                                                                          <a href='https://www.antoinetardivel.com/' target="_blank" className={styles.link}> Antoine Tardivel</a> ,
-                                                                          <a href='https://clairerieusset.fr/' target="_blank" className={styles.link}> Claire Rieuset</a> ,
-                                                                          <a href='https://valentingenest.fr/' target="_blank" className={styles.link}> Valentin Genest</a> et 
-                                                                          <a href='https://maellerabouan.com/' target="_blank" className={styles.link}> Maëlle Rabouan</a>
+              <p className={styles.homeprenom}>Expérience réalisée par :  <a href='https://www.justinemagry.fr/' rel="noreferrer" target="_blank" className={styles.link}> Justine Magry</a> , 
+                                                                          <a href='https://hugoblanchard.com/fr/index-fr.html' rel="noreferrer" target="_blank" className={styles.link}> Hugo Blanchard</a> , 
+                                                                          <a href='https://www.antoinetardivel.com/' rel="noreferrer" target="_blank" className={styles.link}> Antoine Tardivel</a> ,
+                                                                          <a href='https://clairerieusset.fr/' rel="noreferrer"  target="_blank" className={styles.link}> Claire Rieuset</a> ,
+                                                                          <a href='https://valentingenest.fr/' rel="noreferrer" target="_blank" className={styles.link}> Valentin Genest</a> et 
+                                                                          <a href='https://maellerabouan.com/' rel="noreferrer" target="_blank" className={styles.link}> Maëlle Rabouan</a>
                                                                           </p>
               <svg xmlns="http://www.w3.org/2000/svg" width="100" height="1" viewBox="0 0 100 1"><defs><style></style></defs><line className="a" x1="100" transform="translate(0 0.5)"/></svg>
             </div>
